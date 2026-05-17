946028d510f12fdb82e3ed98bf65addfb3b17127
ansh46033512 <patelansh6989@gmail.com>
2026-05-17 11:24:42 +0530

    production error solved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,5 @@
 import { lazy, Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-// import { Home } from './pages/Home'
-// import { Layout } from './pages/Layout'
-// import { Dashboard } from './pages/Dashboard'
-// import { WriteArticle } from './pages/WriteArticle'
-// import { BlogTitles } from './pages/BlogTitles'
-// import { GenerateImages } from './pages/GenerateImages'
-// import { RemoveBackground } from './pages/RemoveBackground'
-// import { RemoveObject } from './pages/RemoveObject'
-// import { ReviewResume } from './pages/ReviewResume'
-// import { Community } from './pages/Community'
 import { Toaster } from "react-hot-toast"
 const Home = lazy(() => import("./pages/Home").then(module => ({default: module.Home})))
 const Layout = lazy(() => import("./pages/Layout").then(module => ({default: module.Layout})))
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import { Navbar } from '../components/Navbar'
 import { Hero } from '../components/Hero'
-import { AITools } from '../components/AITools'
+import { AITools } from '../components/AiTools'
 import { Testimonial } from '../components/Testimonial'
 import { Plan } from '../components/Plan'
 import { Footer } from '../components/Footer'
